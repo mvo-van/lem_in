7e8f13d5b6efef0c9a8d7da1f2d1f162c5650143
toto <gfdgf>
2020-02-13 13:36:25 +0100

    main change

--- src/lem_in.c
+++ src/lem_in.c
@@ -6,7 +6,7 @@
 /*   By: mvo-van- <mvo-van-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/17 20:24:35 by hmidoun           #+#    #+#             */
-/*   Updated: 2020/02/10 18:40:20 by mvo-van-         ###   ########.fr       */
+/*   Updated: 2020/02/13 13:35:00 by mvo-van-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,29 +53,37 @@ int		ft_tab_salle(t_node *salle, t_graph *graph)
 	return (1);
 }
 
+int		free_all(t_graph *graph, t_node *salle)
+{
+	ft_free(NULL, &salle, NULL, 0);
+	free(graph->tab_nodes);
+	free_graph(graph);
+	return (0);
+}
+
 int		main(void)
 {
 	t_graph		graph;
 	t_node		*salle;
-	int			ret;
 
-	ret = 1;
 	salle = NULL;
 	if (ft_parsing(&salle, &((graph).links), &graph) == FLAG_ERREUR)
 		return (0);
-	ft_tab_salle(salle, &graph);
-	if (!set_matrix(&graph) || !(ret = optimal_paths(&graph)))
+	if (!ft_tab_salle(salle, &graph))
 	{
-		if (!ret)
-			ft_putstr("ERROR\n");
 		ft_free(NULL, &salle, NULL, 0);
-		free(graph.tab_nodes);
-		free_graph(&graph);
 		return (0);
 	}
+	if (!set_matrix(&graph))
+	{
+		ft_free(NULL, &salle, NULL, 0);
+		free(graph.tab_nodes);
+	}
+	if (!optimal_paths(&graph))
+	{
+		ft_putstr("ERROR\n");
+		return (free_all(&graph, salle));
+	}
 	output_algo(graph);
-	free_graph(&graph);
-	ft_free(NULL, &salle, NULL, 0);
-	free(graph.tab_nodes);
-	return (0);
+	return (free_all(&graph, salle));
 }
